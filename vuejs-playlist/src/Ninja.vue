f9b18793c821df03dadc65de57bbbaa006b0bb74
Himanshu-Rawat-008 <himanshurawat008.hr@gmail.com>
2021-09-03 10:49:10 +0530

    NEsting Components

---
+++ vuejs-playlist/src/Ninja.vue
@@ -0,0 +1,22 @@
+<template>
+  <div >
+   <ul>
+      <li>Yoshi</li>
+      <li>Some</li>
+      <li>Things</li>
+
+   </ul>    
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
